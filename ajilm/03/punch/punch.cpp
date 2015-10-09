eed64937fb8d5767278340dc500afb80add64d91
Mohammed Ajil <maj@fp-robotics.com>
2015-10-09 12:49:35 +0200

    punch still buggy

--- ajilm/03/punch/punch.cpp
+++ ajilm/03/punch/punch.cpp
@@ -15,45 +15,40 @@ int main(){
                         volume[i] = tmp_vol;
                 }
                 vector<vector<int> > table(num_bev+1, vector<int>(litres+1));
+                vector<bool> used_bev(num_bev+1,false);
 
                 //Initlialize DP Table
                 for(int i = 0; i < num_bev; i++){
                         table[i][0] = 0; //0 litres cost nothing
                 }
                 for(int j = 0; j < litres; j++){
-                        table[0][j] = 100000; //0 beverages are not possible
+                        table[0][j] = 1000000; //0 beverages are not possible
                 }
+
+
                 //Fill DP Table
-                int diff_bevs = 0;
                 for(int i = 1; i <= num_bev; i++){
                         for(int j = 1; j <= litres; j++){
                                 if(j-volume[i] >= 0){
-                                        if(cost[i] + table[i][j-volume[i]] <= table[i-1][j]){
-                                                table[i][j] = cost[i] + table[i][j-volume[i]];
-                                                diff_bevs++;
-                                        } else {
-                                                table[i][j] = table[i-1][j];
-                                        }
-                                        //table[i][j] = min(cost[i] + table[i][j-volume[i]], table[i-1][j]);
+                                        table[i][j] = min(cost[i] + table[i][j-volume[i]], table[i-1][j]);
+                                        if(table[i][j] == cost[i] + table[i][j-volume[i]]) used_bev[i] = true;
                                 } else {
                                         table[i][j] = table[i-1][j];
                                 }
                         }
                 }
-                /*print table
-                for (int i = 0; i <= num_bev; i++){
-                        for(int j = 0; j <= litres; j++){
-                                cout << table[i][j] << " ";
-                        }
-                        cout << endl;
+
+                //Count different beverages
+                int diff_bevs = 0;
+                for(int i = 0; i <= num_bev; i++){
+                        if(used_bev[i]) diff_bevs++;
                 }
-                */
+
+                //Find minimal cost
                 int min_cost = 1000000;
-                //int num_bevs = 0;
                 for(int i = 1; i < num_bev; i++){
-                        if (min_cost >= table[i][litres] && table[i][litres] > 0) {
+                        if (min_cost >= table[i][litres]) {
                                 min_cost = table[i][litres];
-                                //num_bevs = i;
                         }
                 }
                 cout << min_cost << " " << diff_bevs << endl;
