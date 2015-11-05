126df02fe7109e94e46761a9f5678b026182287d
Daniel Maag <danielmaag@gmail.com>
2015-11-06 00:14:22 +0100

    test

---
+++ dmaag/project4/navigation.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+   int t; /*t =< 20*/
+   cin >> t;
+   while(t--) {
+      int n; /*1 =< n =< 30*/
+      int m; /*0 =< m =< 435*/
+      int s; /*starting point, s != t, s >= 0*/
+      int t; /*destination point, t =< n - 1*/
+      cin >> n >> m >> s >> t;
+      for(int i = 0; i < m; i++) {
+         
+      }
+   }
+   return 0;
+}
