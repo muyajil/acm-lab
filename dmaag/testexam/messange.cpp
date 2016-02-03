f118dab8be0bf0efec875ba4948dd4a0e4da78c4
dmaag <daniel.maag@soz.gess.ethz.ch>
2016-02-03 14:53:47 +0100

    2

---
+++ dmaag/testexam/messange.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int tests;
+    cin >> tests;
+    while(tests--) {
+        int n, m;
+        cin >> n >> m;
+        for(int i = 0; i < m; i++) {
+            int u, v;
+            cin >> u >> v;
+            
+        }
+    }
+    return 0;
+}
